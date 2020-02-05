7ffbbe4ae61b8e03229179eaa1b065b6b3088ab2
Maimell <malinka77@gmail.com>
2020-02-05 10:33:00 -0700

    Updates

---
+++ precache-manifest.7dd979556265c8912953c93f9ef73bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e086ce35c5dc166606dc078db77776a",
+    "url": "/Roulette-Game-React/index.html"
+  },
+  {
+    "revision": "400634fecdfb797916fe",
+    "url": "/Roulette-Game-React/static/css/main.f0845007.chunk.css"
+  },
+  {
+    "revision": "8f3a4854acf0425a1137",
+    "url": "/Roulette-Game-React/static/js/2.b1ba421b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Roulette-Game-React/static/js/2.b1ba421b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "400634fecdfb797916fe",
+    "url": "/Roulette-Game-React/static/js/main.36651d3f.chunk.js"
+  },
+  {
+    "revision": "17b476f31a2fa1b501f5",
+    "url": "/Roulette-Game-React/static/js/runtime-main.e0ef3880.js"
+  }
+]);
